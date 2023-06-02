5dfb65d68dea993e8a64605c116c41193b851a62
Camilly Beatriz <camillybeatriz2017@gmail.com>
2023-06-02 11:08:38 -0300

    Implementando serviço

--- api/src/main/java/br/com/projeto/api/controle/Controle.java
+++ api/src/main/java/br/com/projeto/api/controle/Controle.java
@@ -3,6 +3,9 @@ package br.com.projeto.api.controle;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.projeto.api.modelo.Pessoa;
 import br.com.projeto.api.repositorio.Repositorio;
+import br.com.projeto.api.servico.Servico;
 
 @RestController
 public class Controle {
@@ -19,24 +23,106 @@ public class Controle {
     @Autowired
     private Repositorio acao;
 
+    @Autowired
+    private Servico servico;
+
     @PostMapping("/api")
+    public ResponseEntity<?> cadastrar(@RequestBody Pessoa obj){
+        return servico.cadastrar(obj);
+    }
+
+    /*@PostMapping("/api")
     public Pessoa cadastrar(@RequestBody Pessoa obj){
         return acao.save(obj);
-    }
+    }*/
 
     @GetMapping("/api")
-    public List<Pessoa> selecionar(){
-        return acao.findAll();
+    public ResponseEntity<?> selecionar(){
+        return servico.selecionar();
     }
 
-    @GetMapping("/api/{codigo}")
+    /*@GetMapping("/api")
+    public List<Pessoa> selecionar(){
+        return acao.findAll();
+    }*/
+
+    /*@GetMapping("/api/{codigo}")
     public Pessoa selecionarPeloCodigo(@PathVariable int codigo){
         return acao.findByCodigo(codigo);
+    }*/
+
+    @GetMapping("/api/{codigo}")
+    public ResponseEntity<?> selecionarPeloCodigo(@PathVariable int codigo){
+        return servico.selecionarPeloCodigo(codigo);
     }
 
     @PutMapping("/api")
+    public ResponseEntity<?> editar(@RequestBody Pessoa obj){
+        return servico.editar(obj);
+    }
+
+    /*@PutMapping("/api")
     public Pessoa editar(@RequestBody Pessoa obj){
         return acao.save(obj);
+    }*/
+
+    @DeleteMapping("/api/{codigo}")
+    public ResponseEntity<?> remover(@PathVariable int codigo){
+        return servico.remover(codigo);
+    }
+
+    /*@DeleteMapping("/api/{codigo}")
+    public void remover(@PathVariable int codigo){
+        //Pessoa obj = selecionarPeloCodigo(codigo);
+
+        //acao.delete(obj);
+    }*/
+
+    @GetMapping("/api/contador")
+    public long contador(){
+        return acao.count();
+    }
+
+    // Ordenar de forma crescente
+    /*@GetMapping("/api/ordenarNomes")
+    public List<Pessoa> ordenarNomes(){
+        return acao.findByOrderByNomeAsc();
+    }*/
+
+    //Ordenar de forma decrescente 
+    @GetMapping("/api/ordenarNomes")
+    public List<Pessoa> ordenarNomes(){
+        return acao.findByOrderByNomeDesc();
+    }
+
+    @GetMapping("/api/ordenarNomes2")
+    public List<Pessoa> ordenarNomes2(){
+        return acao.findByNomeOrderByIdadeDesc("Camilly");
+    }
+
+    @GetMapping("/api/nomeContem")
+    public List<Pessoa> nomeContem(){
+        return acao.findByNomeContaining("Lu");
+    }
+
+    @GetMapping("/api/iniciaCom")
+    public List<Pessoa> iniciaCom(){
+        return acao.findByNomeStartsWith("a");
+    }
+
+    @GetMapping("/api/terminaCom")
+    public List<Pessoa> terminaCom(){
+        return acao.findByNomeEndsWith("a");
+    }
+
+    @GetMapping("/api/somaIdades")
+    public int somaIdades(){
+        return acao.somaIdades();
+    }
+
+    @GetMapping("/api/idadeMaiorIgual")
+    public List<Pessoa> idadeMaiorIgual(){
+        return acao.idadeMaiorIgual(25);
     }
 
     @GetMapping("")
@@ -57,5 +143,10 @@ public class Controle {
     @PostMapping("/pessoa")
     public Pessoa pessoa(@RequestBody Pessoa p){
         return p;
+    }
+
+    @GetMapping("/status")
+    public ResponseEntity<?> status(){
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
