ab77e93ebb8c7383ba321111064170d5921c5779
Camilly Beatriz <camillybeatriz2017@gmail.com>
2023-06-06 09:07:27 -0300

    projeto finalizado

--- api/src/main/java/br/com/projeto/api/validacao/Validacao.java
+++ api/src/main/java/br/com/projeto/api/validacao/Validacao.java
@@ -7,7 +7,6 @@ package br.com.projeto.api.validacao;
  // Importar http, validation, bind, context e servlet
  import org.springframework.http.HttpHeaders;
  import org.springframework.http.HttpStatus;
- import org.springframework.http.HttpStatusCode;
  import org.springframework.http.ResponseEntity;
  import org.springframework.validation.FieldError;
  import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -16,21 +15,20 @@ package br.com.projeto.api.validacao;
  import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
  @ControllerAdvice
- public class Validacao extends ResponseEntityExceptionHandler {
+ public class Validacao extends ResponseEntityExceptionHandler{
+     
+     @Override
+     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+             HttpHeaders headers, HttpStatus status, WebRequest request) {
+         
+         Map<String, String> errors = new HashMap<>();
+         ex.getBindingResult().getAllErrors().forEach((error) ->{
+             
+             String fieldName = ((FieldError) error).getField();
+             String message = error.getDefaultMessage();
+             errors.put(fieldName, message);
+         });
+         return new ResponseEntity<Object>(errors, HttpStatus.BAD_REQUEST);
+     }
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-            HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-
-            Map<String, String> errors = new HashMap<>();
-            ex.getBindingResult().getAllErrors().forEach((error) -> {
-
-              String fieldName = ((FieldError) error).getField();
-              String message = error.getDefaultMessage();
-              errors.put(fieldName, message);
-            });
-
-          return new ResponseEntity<Object>(errors, HttpStatus.BAD_REQUEST);
-      }
-
-  }
+ }
